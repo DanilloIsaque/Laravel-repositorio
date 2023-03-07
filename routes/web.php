98e16f74495cec40adf8d7cb7f816e492112d1b7
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-03-06 22:29:08 -0300

    Commit
    Apresentação de Laravel, usamos para fazer a conexão com o banco de dados.

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\http\Controllers\ProdutoController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/produto',[ProdutoController::class,'index']);
