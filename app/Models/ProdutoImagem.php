2597a6364716759a5f575f54ad92f11de9a26d41
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-03-20 21:00:15 -0300

    commit

--- app/Models/ProdutoImagem.php
+++ app/Models/ProdutoImagem.php
@@ -5,10 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Produto extends Model
-
+class ProdutoImagem extends Model
 {
     use HasFactory;
-    protected $table = "PRODUTO";
-    protected $primaryKey = "PRODUTO_ID";
+    protected $table = "PRODUTO_IMAGEM";
 }
